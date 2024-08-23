467717d0607e05702c51f1449f841a96b5e22a5b
anikibengal <128947315+anikibengal@users.noreply.github.com>
2024-08-23 13:12:44 +0800

    Update GPR_Dashboard.py

--- GPR_Dashboard.py
+++ GPR_Dashboard.py
@@ -21,10 +21,9 @@ st.markdown(
 )
 
 # 資料路徑
-data_path = r'C:\Users\user\Desktop\GPR\data\latest_data.xlsx'
-transposed_data_path = r'C:\Users\user\Desktop\GPR\data\transposed_data.xlsx'
+data_path = "/data/latest_data.xlsx"
+transposed_data_path = "data/transposed_data.xlsx"
 data_gpr_daily_recent = "https://www.matteoiacoviello.com/gpr_files/data_gpr_daily_recent.xls"
-#r'C:\Users\user\Desktop\GPR\data\data_gpr_daily_recent.xls'
 
 # 設置儀表板標題
 title_col, emp_col = st.columns([1, 0.2])
